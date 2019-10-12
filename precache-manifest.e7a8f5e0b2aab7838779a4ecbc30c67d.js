474de008ecd830f83bf6644fc371cecd89f97adc
Viktor2350 <ryjuk39@gmail.comm>
2019-10-12 17:10:48 +0300

    Updates

---
+++ precache-manifest.e7a8f5e0b2aab7838779a4ecbc30c67d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1791d42db2010c37f3ec85ec41d4152",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "81a4aec832bc9e21d42b",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.93f42686.chunk.css"
+  },
+  {
+    "revision": "0334c63d8d8247022bb0",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.460ba90e.chunk.js"
+  },
+  {
+    "revision": "81a4aec832bc9e21d42b",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.61d723a9.chunk.js"
+  },
+  {
+    "revision": "aea2d0b96aafbd415263",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.f9462026.js"
+  }
+]);
